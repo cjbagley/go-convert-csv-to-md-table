cab672282ec893bc44035b7ffd01f606aa7763c9
Colin Bagley <cjbagley@googlemail.com>
2023-10-03 18:58:16 +0100

    WIP - working on reading file

--- main.go
+++ main.go
@@ -5,33 +5,51 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	path "path/filepath"
 )
 
 func main() {
+	noHeader := flag.Bool("no-header", false, "Pass if no header row (otherwise defaults to assuming one is present)")
 	flag.Parse()
-	filepath := flag.Arg(0)
-	if filepath == "" {
-		fmt.Println("Please specify a file to convert ")
-		os.Exit(1)
+
+	readFile := flag.Arg(0)
+	if readFile == "" {
+		exit("Please specify a file to convert")
 	}
 
-	file, err := os.Open(filepath)
+	dir := path.Dir(readFile)
+	readFilename := path.Base(readFile)
+
+	file, err := os.Open(readFile)
 	defer file.Close()
 
 	if err != nil {
-		fmt.Println(fmt.Errorf("Could not open '%s': %w", filepath, err))
-		os.Exit(1)
+		exit(fmt.Errorf("Could not open '%s': %v", readFile, err).Error())
 	}
+
+	writeFile, err := os.Create(dir + "/updated-" + readFilename)
 
 	reader := csv.NewReader(file)
-	var records [][]string
-	records, err = reader.ReadAll()
-	if err != nil {
-		fmt.Println(fmt.Errorf("Failed to read contents of '%s': %w", filepath, err))
-		os.Exit(1)
+	// writer := csv.NewWriter(writeFile)
+	defer writeFile.Close()
+
+	var record []string
+	if *noHeader == false {
+		record, err = reader.Read()
+		fmt.Println(record)
 	}
 
-	for _, record := range records {
+	for {
+		record, err = reader.Read()
+		if err != nil {
+			exit(fmt.Errorf("Failed to read contents of '%s': %w", readFile, err).Error())
+		}
 		fmt.Println(fmt.Printf("%s", record))
+		break
 	}
+}
+
+func exit(message string) {
+	fmt.Println(message)
+	os.Exit(1)
 }
