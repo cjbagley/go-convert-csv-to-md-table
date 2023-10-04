63940625e2f155f59a1521de36d75ecddd2dbd74
Colin Bagley <cjbagley@googlemail.com>
2023-10-04 11:42:41 +0100

    WIP - working on writing to file

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	path "path/filepath"
 )
@@ -28,28 +29,45 @@ func main() {
 	}
 
 	writeFile, err := os.Create(dir + "/updated-" + readFilename)
+	defer writeFile.Close()
 
 	reader := csv.NewReader(file)
-	// writer := csv.NewWriter(writeFile)
-	defer writeFile.Close()
+	writer := csv.NewWriter(writeFile)
+	defer writer.Flush()
 
 	var record []string
 	if *noHeader == false {
 		record, err = reader.Read()
-		fmt.Println(record)
+        writer.Write(processRow(record))
 	}
 
 	for {
 		record, err = reader.Read()
+		if err == io.EOF {
+			fmt.Println("Successfully created")
+			break
+		}
+
 		if err != nil {
 			exit(fmt.Errorf("Failed to read contents of '%s': %w", readFile, err).Error())
 		}
-		fmt.Println(fmt.Printf("%s", record))
-		break
+
+        writer.Write(processRow(record))
 	}
 }
 
 func exit(message string) {
 	fmt.Println(message)
 	os.Exit(1)
+}
+
+func processRow(row []string) []string {
+    firstColumn := true 
+    for _, r := range row {
+        if firstColumn {
+            
+        }
+		fmt.Println(r)
+	}
+	return row
 }
